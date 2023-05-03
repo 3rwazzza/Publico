521ec10fc09126e225e58b55e2cc80a254fcbf87
1º DAM DCP <1 DAM DCP@DESKTOP-K0UNRD6>
2023-05-03 21:00:57 +0200

    tercero

--- Proyecto figuras/figuras/Cuadrado.java
+++ Proyecto figuras/figuras/Cuadrado.java
@@ -6,6 +6,6 @@ public class Cuadrado extends Rectángulo{
 
 public Cuadrado (double x, double y, Color color, double lado){
 	
-super (x, y, bol, lado, lado);
+super (x, y, color, lado, lado);
 }
 }
